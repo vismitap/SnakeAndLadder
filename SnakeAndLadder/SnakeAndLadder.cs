240ccc343e94a149f8ae6b33d8cc63345e993d2c
vismitap <vismita.prabhu@gmail.com>
2022-01-30 15:59:14 +0530

    [Vismita] Add: UC3 solved

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -8,13 +8,33 @@ namespace SnakeAndLadder
 {
     internal class SnakeAndLadder
     {
+        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        int playerPosition = 0;
         public void Play()
         {
-            int startPosition = 0;
             Console.WriteLine("The game has started");
             Random random = new Random();
             int diceOutcome = random.Next(1,7);
             Console.WriteLine("The dice outcome is: "+diceOutcome);
+            
+            int option = random.Next(0,3);
+
+            switch (option)
+            {
+                case NO_PLAY:
+                    break;
+                    
+                case LADDER:
+                    playerPosition += diceOutcome;
+                    break;
+
+                case SNAKE:
+                    playerPosition -= diceOutcome;
+                    break;
+
+            }
+
+            Console.WriteLine("Option is: "+ option +"\nPlayer position is: " + playerPosition);
         }
     }
 }
